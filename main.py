49a0543608bdfd7b6eb033893f68ee43b604d44e
Jun Maeda <50643679+Jun-Maeda@users.noreply.github.com>
2021-08-01 02:27:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 import json
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
+from covit_news import info_get
 
 if __name__ == "__main__":
     info_get()
